f0bf1e4235b6692fadb7f3dbade239fdb982c449
Dave Hawthorne <davehawthorne@ieee.org>
2017-03-30 15:29:52 -0400

    Use 3rd party library for ADS1115 control
    Also build a class to manage the AM416.

--- sketches/am416/am416.ino
+++ sketches/am416/am416.ino
@@ -1,122 +1,118 @@
 
-#include "RTClib.h"
-#include <Wire.h>
-
+// Example Coweeta data logger sketch 
+//
+// This uses:
+// * an Adafruit 16 bit analog to digital converter, 
+// * an Adafruit temperature sensor and
+// * a Campbell Scientific relay multiplexer.
+//
+//
 #include <Adafruit_ADS1015.h>
+#include <Adafruit_MCP9808.h>
 
 #include "data_logger.h"
 
 using namespace coweeta;
 
-#define MCP9808_I2CADDR_DEFAULT        0x18
-#define MCP9808_REG_CONFIG             0x01
 
-#define MCP9808_REG_CONFIG_SHUTDOWN    0x0100
-#define MCP9808_REG_CONFIG_CRITLOCKED  0x0080
-#define MCP9808_REG_CONFIG_WINLOCKED   0x0040
-#define MCP9808_REG_CONFIG_INTCLR      0x0020
-#define MCP9808_REG_CONFIG_ALERTSTAT   0x0010
-#define MCP9808_REG_CONFIG_ALERTCTRL   0x0008
-#define MCP9808_REG_CONFIG_ALERTSEL    0x0004
-#define MCP9808_REG_CONFIG_ALERTPOL    0x0002
-#define MCP9808_REG_CONFIG_ALERTMODE   0x0001
+class Am416 {
+public:
+  Am416();
+  void setup(uint8_t channels, uint8_t clk_pin, uint8_t res_pin);
+  void read_all(void (*callback)(uint8_t channel));
+private:
+  uint8_t _channels;
+  uint8_t _clk_pin;
+  uint8_t _res_pin;
+  void (*_callback)(uint8_t channel);
+};
 
-#define MCP9808_REG_UPPER_TEMP         0x02
-#define MCP9808_REG_LOWER_TEMP         0x03
-#define MCP9808_REG_CRIT_TEMP          0x04
-#define MCP9808_REG_AMBIENT_TEMP       0x05
-#define MCP9808_REG_MANUF_ID           0x06
-#define MCP9808_REG_DEVICE_ID          0x07
-
-
-static void i2c_write16(uint8_t i2c_addr, uint8_t reg, uint16_t value) {
-    Wire.beginTransmission(i2c_addr);
-    Wire.write(reg);
-    Wire.write(value >> 8);
-    Wire.write(value & 0xFF);
-    Wire.endTransmission();
+Am416::Am416()
+{
 }
 
-static uint16_t i2c_read16(uint8_t i2c_addr, uint8_t reg) {
-  uint16_t val;
-
-  Wire.beginTransmission(i2c_addr);
-  Wire.write(reg);
-  Wire.endTransmission();
-
-  Wire.requestFrom(i2c_addr, (uint8_t)2);
-  val = Wire.read();
-  val <<= 8;
-  val |= Wire.read();
-  return val;
+void Am416::setup(uint8_t channels, uint8_t clk_pin, uint8_t res_pin)
+{
+  _channels = channels;
+  _clk_pin = clk_pin;
+  _res_pin = res_pin;
+  pinMode(_clk_pin, OUTPUT);
+  pinMode(_res_pin, OUTPUT);
 }
 
-static Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+
+void Am416::read_all(void (*callback)(uint8_t channel))
+{
+  digitalWrite(_clk_pin, LOW);
+  digitalWrite(_res_pin, HIGH);
+
+  for (uint8_t i = 0; i < _channels; i++) {
+      delay(5);
+      digitalWrite(_clk_pin, HIGH);
+      delay(10);
+      callback(i);
+      digitalWrite(_clk_pin, LOW);
+    }
+    digitalWrite(_res_pin, LOW);
+}
+
+static Adafruit_ADS1115 ads;  // Our 16 bit ADC
+static Adafruit_MCP9808 mcp;  // Our temperature probe
+static Am416 am;   
+
 static DataLogger logger;
 
 
 
-const int am_rst = 7;
-const int am_clk = 6;
-
 enum {
-  alfa_read = 1,
-  bravo_read_1 = 2,
-  bravo_read_2 = 4,
-  bravo_energize = 8
+  temp_read = 1,
+  adc_read = 2
 };
 
-// Declare an array of structures that define what our schedule of events is.
-// The alfa_read event occurs every minute, on the minute.
-// The bravo events occur every five minutes.  The energize event occurs
-// 10 seconds before the start of the minute, then read 1 occurs on the minute,
-// followed 10 seconds later by read 2.
+
 static const EventSchedule schedule[] = {
-  event("MCP9808", HMS(0, 1, 0)),  
-  event("AM416", HMS(0, 0, 30))
+  event("temp_read", HMS(0, 1, 0)),
+  event("adc_read", HMS(0, 0, 30))
 };
 
 void setup() {
   logger.setup();
   logger.set_schedule(schedule, 2);
 
-  // set up multiplexer control
-  pinMode(am_rst, OUTPUT);
-  pinMode(am_clk, OUTPUT);
-
-  ads.setGain(GAIN_ONE);
+  am.setup(16, 6, 7);
+  
   ads.begin();
+  ads.setGain(GAIN_SIXTEEN);
 
+  mcp.begin();
+}
+
+
+static void read_channel(uint8_t channel)
+{
+  const int16_t diff = ads.readADC_Differential_0_1();
+  logger.log_int(diff);
 }
 
 
 
-void loop() 
+void loop()
 {
 
   logger.wait_for_event();
+  
   logger.new_log_line();
-  if (logger.is_event(alfa_read)) {
-      const uint16_t t_reg = i2c_read16(MCP9808_I2CADDR_DEFAULT, MCP9808_REG_AMBIENT_TEMP);
-      const int16_t temp = (t_reg & 0x0FFF) - (t_reg & 0x1000);
-      logger.log_int(temp);
+  if (logger.is_event(temp_read)) {
+      float temp = mcp.readTempC();
+      logger.log_float(temp, 1);
   } else {
       logger.skip_entries(1);
   }
-  if (logger.is_event(bravo_read_1)) {
-    digitalWrite(am_clk, LOW);
-    digitalWrite(am_rst, HIGH);
-
-    for (int i = 0; i < 16; i++) {
-      delay(5);
-      digitalWrite(am_clk, HIGH);
-      delay(10);
-      const int16_t diff = ads.readADC_Differential_0_1();
-      logger.log_int(diff);
-      digitalWrite(am_clk, LOW);
-    }
-    digitalWrite(am_rst, LOW);
+  
+  if (logger.is_event(adc_read)) {
+    am.read_all(read_channel);
   }
+  
   logger.end_log_line();
 }
 
